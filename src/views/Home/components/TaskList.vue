c062ca2663154abef6d5bfcab02a0ef55c3cde27
Rona <142495887+rona94@users.noreply.github.com>
2024-04-25 02:08:14 +0800

    update code

---
+++ src/views/Home/components/TaskList.vue
@@ -0,0 +1,96 @@
+<template>
+  <div ref="task" class="home task-manager-wrap">
+    <div v-for="status in tasksStatus" :key="status.id" class="task-manager" :data-status="status.status" :class="`task-manager--${status.status}`">
+        <div class="box">
+          <div class="box__title">{{ status.name }}</div>
+          <div class="box__content">
+            <template v-for="item in data" :key="item.id">
+              <TaskList :id="item.id" :text="item.name" v-if="item.status == status.status" @click="openModal(item.id)" />
+            </template>
+          </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+import TaskList from '@/components/TaskList.vue'
+
+export default {
+  props: ["data"],
+  components: {
+    TaskList,
+  },
+  computed: {
+    ...mapGetters({tasksStatus: "getTasksStatus"})
+  },
+  watch: {
+    data() {
+      this.$nextTick(() => {
+        setTimeout(() => {
+          this.onLoad()
+        }, 100)
+      })
+    },
+  },
+  methods: {
+    ...mapActions(["fetchTasksStatus", "updateTask"]),
+    async updateData(id, status) {
+      const data = {
+        id,
+        data: { status }
+      }
+      this.updateTask(data)
+    },
+    openModal(value) {
+      this.$emit("openModal", value)
+    },
+    onLoad() {
+      let task = this.$refs.task.querySelectorAll(".box__content")
+      let taskList = this.$refs.task.querySelectorAll(".task-list");
+
+      taskList.forEach(element => {
+        element.addEventListener('dragstart', this.onDragStart);
+      })
+
+      task.forEach(element => {
+        element.addEventListener("dragover", this.onDragOver);
+        element.addEventListener("drop", this.onDrop);
+      })
+    },
+    onDragStart(e) {
+      e.dataTransfer.clearData();
+      e.dataTransfer.setData("text/plain", e.target.id);
+    },
+    onDragOver(e) {
+      e.preventDefault();
+    },
+    onDrop(e) {
+      e.preventDefault();
+      
+      const data = e.dataTransfer.getData("text");
+      const source = document.getElementById(data);
+
+      const id = data.replace("task-list--", "")
+      const status = e.target.closest(".task-manager").getAttribute("data-status")
+      this.updateData(id, status)
+
+      if(e.target.classList.contains('task-list')) {
+        if(e.offsetY <= e.target.offsetHeight/2) {
+          e.target.parentNode.insertBefore(source, e.target)
+        }
+        else {
+          e.target.parentNode.insertBefore(source, e.target.nextSibling)
+        }
+      }
+      else {
+        e.currentTarget.appendChild(source);
+      }
+    },
+  },
+  mounted() {
+    this.fetchTasksStatus()
+  }
+}
+</script>
